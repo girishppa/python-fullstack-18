7802587ef177ce0afcffbb382b3981a333b7423b
Akshata-Bhoite-Git <akshatabhoite0101@gmail.com>
2026-03-16 07:52:54 +0530

    Add New data from akshata

---
+++ team/akshata/Python_Batch18_New/Day1.py/Modues.py
@@ -0,0 +1,27 @@
+
+#write a program random module
+
+import random
+
+#randint function give random number
+#randint(start , stop)
+num = random.randint(1,10)
+print(num)
+
+
+#randrange(start , stop , step)
+num1 = random .randrange(1, 10 ,2)
+print(num1)
+
+
+#import date and time
+import datetime
+
+current_time = datetime.datetime.now()
+print(current_time)
+
+
+
+
+
+
---
+++ team/akshata/Python_Batch18_New/Day1.py/User_details_from_input.py
@@ -0,0 +1,8 @@
+
+#Take name and age from user
+
+user_name = input("Please Enter Your Name : ")
+user_age = input("Please Enter Your Age: ")
+
+#print name and age 
+print("Hello "+ user_name + ", you are "+ user_age + " years old")
