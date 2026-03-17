dbdbb5096f2e55c4f0e9013cb4af67b7239e4322
girishppa <shetegirish123@gmail.com>
2026-03-17 10:06:39 +0530

    girish code

---
+++ girish/gs.py
@@ -0,0 +1,28 @@
+# def girish():
+#      print("a+b")
+
+# girish()
+# girish()
+# girish()
+
+
+# def addition(first_number,second_number):
+
+#     sum= first_number + second_number
+#     print(sum)
+
+    
+# addition(4,6) 
+
+
+def pi():
+    return 3.142857
+
+
+
+def area_of_circle(radius):
+    return radius*radius*pi()
+
+
+print(area_of_circle(12))
+
